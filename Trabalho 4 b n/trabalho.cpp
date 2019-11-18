f90ce6365bd21bab6d77ec62b9eac7ebb10c027f
Gabriel112112 <57267860+Gabriel112112@users.noreply.github.com>
2019-11-18 15:29:17 -0300

    Add files via upload

---
+++ Trabalho 4 b n/trabalho.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+
+/*
+	Conta todas as letras 'letra' contidas em 'texto'
+*/
+int contaLetras(char letra, string texto)
+{
+  int cont, i;
+  for(i=0;i<texto.length();i++)
+  {
+      if(texto[i]==letra)
+      cont++;
+  }
+  return cont;
+}
+
+/*
+	Substitui todas as ocorrencias de 'letra1' por 'letra2' contidas em 'texto'
+*/
+void substituirLetra(char letra1,char letra2, string texto)
+{
+    char string[100];
+    int x, tam;
+    tam=strlen(string);
+    for (x=0;x<tam;x++)
+        {
+        if (string[x]=letra1)
+        {
+            letra1=letra2;
+        }
+    }
+
+}
+
+/*
+	Conta todas as ocorrencias de 'palavra' contidas em 'texto'
+*/
+int contaPalavras(string palavra, string texto)
+{
+}
+
+/*
+	Substitui todas as ocorrencias de 'palavra1' por 'palavra2' em 'texto'
+*/
+void substituirPalavras(string palavra1, string palavra2, string texto)
+{
+}
+
+int main(){
+
+	cout << "Entre com o texto para fazer o teste:";
+
+
+
+
+	return 0;
+}
