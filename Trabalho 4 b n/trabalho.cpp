4775293f89bf3c903e1d12b40e1282667a39c630
Rebeca112112 <rebecamarquesrapunzel@gmail.com>
2019-11-18 16:46:43 -0200

    altereiiiii

--- Trabalho 4 b n/trabalho.cpp
+++ Trabalho 4 b n/trabalho.cpp
@@ -38,6 +38,15 @@ void substituirLetra(char letra1,char letra2, string texto)
 */
 int contaPalavras(string palavra, string texto)
 {
+    int cont, i;
+  for(i=0;i<texto.length();i++)
+  {
+      if(texto[i]==palavra)
+      cont++;
+  }
+  return cont;
+
+
 }
 
 /*
@@ -45,11 +54,40 @@ int contaPalavras(string palavra, string texto)
 */
 void substituirPalavras(string palavra1, string palavra2, string texto)
 {
+    char string[100];
+    int x, tam;
+    tam=strlen(string);
+    for (x=0;x<tam;x++)
+        {
+        if (string[x]=palavra1)
+        {
+            palavra1=palavra2;
+        }
+    }
+
+
 }
 
-int main(){
-
+int main()
+{
+    string a,b,c,d;
+    string x,v,n,m;
 	cout << "Entre com o texto para fazer o teste:";
+	cin>>a;
+    x=contaLetras(a);
+    cout<<x;
+    cout << "Entre com o texto para fazer o teste:";
+    cin>>b;
+    v=substituirLetra(b);
+    cout<<v;
+    cout << "Entre com o texto para fazer o teste:";
+    cin>>c;
+    n=contaPalavras(c);
+    cout<<n;
+    cout << "Entre com o texto para fazer o teste:";
+    cin>>d;
+    m=substituirPalavras(d);
+    cout<<m;
 
 
 
